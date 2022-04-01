d775f41afca0b25393696e8cc621805b6e20b98f
Tiago <herique.sa@gmail.com>
2022-04-01 00:28:10 -0300

    habilitando all origins cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,12 @@
 import { setupRoutes } from '@/main/config/routes';
 import express from 'express';
+import cors from 'cors';
 const app = express();
+
+const options: cors.CorsOptions = {
+  origin: '*'
+};
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 setupRoutes(app);
