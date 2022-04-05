f300529eb529abb6217a8af6ad5c70aed1deba5d
Tiago Hérique S. de Almeida <herique.sa@gmail.com>
2022-04-04 21:45:15 -0300

    Update app.ts
    credential

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,12 +2,20 @@ import { setupRoutes } from '@/main/config/routes';
 import express from 'express';
 import cors from 'cors';
 const app = express();
-
-const origin = {
-//  origin: isProduction ? 'https://whitelabel.com' : '*',
-  origin: 'http://localhost:3000',
-  Access-Control-Allow-Credentials: true
-}
+const API_URL = 'http://localhost:3000';
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: API_URL,
+  preflightContinue: false,
+};
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 app.use(cors(origin))
